b160c80254c1239a5fd84360e1705b90c989ead2
razzalok <alok.raj@mindfire.com>
2025-10-06 16:15:51 +0530

    feat(client): add component pages and service for auth

---
+++ src/app/component/nav-links/nav-links.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-links',
+  imports: [],
+  templateUrl: './nav-links.html',
+  styleUrl: './nav-links.css'
+})
+export class NavLinks {
+
+}
