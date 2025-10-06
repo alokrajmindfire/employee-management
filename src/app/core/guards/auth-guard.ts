4f88f22b4bb363279a26dade550c68895f26a655
alok-raj <Alok.Raj@mindfiresolutions.com>
2025-10-06 19:38:27 +0530

    fix(auth): resolve injectable issue for contructor with inject

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,11 +1,13 @@
+import { inject, Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthGuard implements CanActivate {
-  constructor(
-    private authService: Auth,
-    private router: Router,
-  ) {}
+  private authService = inject(Auth);
+  private router = inject(Router);
   canActivate(): boolean {
     if (this.authService.isAuthenticated()) return true;
     this.router.navigate(['/login']);
--- src/app/core/guards/no-auth-guard.ts
+++ src/app/core/guards/no-auth-guard.ts
@@ -1,11 +1,13 @@
+import { inject, Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class NoAuthGuard implements CanActivate {
-  constructor(
-    private authService: Auth,
-    private router: Router,
-  ) {}
+  private authService = inject(Auth);
+  private router = inject(Router);
   canActivate(): boolean {
     if (this.authService.isAuthenticated()) {
       this.router.navigate(['/']);
